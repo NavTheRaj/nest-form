acc0cf48be2ecbdc1edd5507ca76354de9a6f9bb
Navraj <navrajkhanal61@gmail.com>
2021-04-01 22:29:52 +0545

    Added the deployed url in the servuce

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,8 +1,4 @@
-export const countryEndPoint = {
-  countryData: "https://restcountries.eu/rest/v2/all",
-};
-
 export const serverEndPoint = {
-  formSubmit: "http://localhost:3001/users",
-  allData: "http://localhost:3001/users",
+  formSubmit: "https://nestserver.herokuapp.com/users",
+  allData: "https://nestserver.herokuapp.com/users",
 };
